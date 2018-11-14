cce9420126588e56e4d74d76c5b77ae580815bd4
Oli <ojmd@live.de>
2018-11-14 11:41:03 +0100

    neue Ordner Struktur

--- localization.de.lua
+++
@@ -1,6 +0,0 @@
-OJMD_CLOSE          = "Schliessen"  -- Schliessen
-OJMD_BROWSER_NAME   = "Name"        -- Table Name
-OJMD_INI            = "Instanz"     -- Button TEXT Name
-OJMD_RAID           = "Raids"       -- Button TEXT Name
-OJMD_PVP            = "PvP"         -- Button TEXT Name
-OJMD_SCENARIO       = "Expedition"
